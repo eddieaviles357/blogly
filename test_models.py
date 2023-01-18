10e52fcc0465f9b794353516a9a55201c94d1817
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-01-17 20:10:30 -0800

    added an app context and placed db methods in app context, made app more modern" "

--- test_models.py
+++ test_models.py
@@ -1,36 +1,42 @@
 # to run test
-# python3 -m unittest test_models.py
+# SQLALCHEMY_WARN_20=1 python3 -m unittest test_models.py
 from unittest import TestCase
 
 from app import app
 from models import db, User
 
-# Use test database and don't clutter tests with SQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
-app.config['SQLALCHEMY_ECHO'] = False
-
-db.drop_all()
-db.create_all()
-
 
 class UserModelTestCase(TestCase):
-    """Tests for model for Users."""
+    """ Tests Users model """
 
     def setUp(self):
         """Clean up any existing users."""
+        self.client = app.test_client()
+        # use test database
+        app.config.update({
+            "TESTING": True,
+            "SQLALCHEMY_ECHO": False,
+            "SQLALCHEMY_DATABASE_URI": "postgresql:///blogly_test",
+            "DEBUG_TB_HOSTS": ["dont-show-debug-toolbar"]
+        })
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
 
-        User.query.delete()
+            User.query.delete()
 
     def tearDown(self):
         """Clean up any fouled transaction."""
+        with app.app_context():
+            db.session.rollback()
 
-        db.session.rollback()
+    def test_greet(self):
+        user = User(f_name="Ed", l_name="Aviles")
 
-    # def test_greet(self):
-        # user = User(f_name="UserTest", l_name="lastnameuser")
-        # self.assertEquals(user.greet(), "I'm TestPet the dog")
+        self.assertEqual(
+            user.greet(), f"Hi my name is {user.f_name} {user.l_name}!")
 
-    # def test_feed(self):
+    def test_get_all_users_by_first_name(self):
         # user = User(f_name="UserTest", l_name="lastnameuser")
         # user.feed(5)
         # self.assertEquals(user.hunger, 5)
@@ -38,10 +44,10 @@ class UserModelTestCase(TestCase):
         # user.feed(999)
         # self.assertEquals(user.hunger, 0)
 
-    # def test_get_by_species(self):
-    #     user = User(f_name="UserTest", l_name="lastnameuser")
-    #     db.session.add(user)
-    #     db.session.commit()
+        # def test_get_by_species(self):
+        #     user = User(f_name="UserTest", l_name="lastnameuser")
+        #     db.session.add(user)
+        #     db.session.commit()
 
         # dogs = User.get_by_species('dog')
         # self.assertEquals(dogs, [user])
